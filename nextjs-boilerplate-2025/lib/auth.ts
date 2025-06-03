adb786328688ab9b89c347d36d180fdbcfb2ff44
kgogo5 <kgogo9@gmail.com>
2025-06-03 14:54:46 +0900

    초기화 및 재 설치

--- nextjs-boilerplate-2025/lib/auth.ts
+++
@@ -1,7 +0,0 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-
-// 서버에서 세션 가져오기
-export async function getSession() {
-  return await getServerSession(authOptions);
-}
